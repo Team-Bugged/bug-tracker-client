3220b36fe14dc4d267c4675635bfe74ea43502b0
noobie10123 <57508390+noobie10123@users.noreply.github.com>
2022-01-13 20:11:47 +0530

    frontend changes

--- src/components/ProjectsBugsDashboard.js
+++ src/components/ProjectsBugsDashboard.js
@@ -3,6 +3,39 @@ import { getProjects, getBugs } from "./ServerConnections";
 import { ProjectBar } from "./ProjectBar";
 import { BugBar } from "./BugBar";
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function ProjectsBugsDashboard() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+
 const ProjectsBugsDashboard = () => {
   const [projectsArray, setProjectsArray] = useState([]);
   const [bugsArray, setBugsArray] = useState([]);
